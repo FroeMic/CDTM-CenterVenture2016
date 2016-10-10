b3e7d1b23316aadeee2b3bf324575e01afd270e8
Michael Fröhlich <M.Froehlich1994@gmail.com>
2016-10-10 22:45:09 +0200

    .

--- deployment/webhookListener.js
+++ deployment/webhookListener.js
@@ -8,13 +8,17 @@ app.post('/', function(req, res) {
    console.log("Received Stuff");
    console.log(req.body);
 
-   exec('./deploy.sh', function (error, stdout, stderr) {
-      //console.log('stdout: ' + stdout);
-      //console.log('stderr: ' + stderr);
-      //if (error !== null) {
-      // console.log('exec error: ' + error);
-      //}
-   });
+   // only pull if it's the master branch
+   if (req.body != undefined && req.body.ref == 'refs/heads/master') {
+     console.log("Pulling...");
+     exec('./deploy.sh', function (error, stdout, stderr) {
+        //console.log('stdout: ' + stdout);
+        //console.log('stderr: ' + stderr);
+        //if (error !== null) {
+        // console.log('exec error: ' + error);
+        //}
+     });
+   }
 });
 
 app.listen(9999, function(){
