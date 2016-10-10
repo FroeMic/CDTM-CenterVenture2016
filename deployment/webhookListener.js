dbf43c8c5381e8c9123a9042df23c5f826467b7a
Michael Froehlich <mike@frhlch.at>
2016-10-10 19:51:51 +0000

    Update deployment script

--- deployment/webhookListener.js
+++ deployment/webhookListener.js
@@ -1,13 +1,21 @@
-/**
- * Global Module dependencies.
- */
-require('coffee-script');
-
 var express = require('express');
 var app = module.exports = express();
+var exec = require('child_process').exec;
+
 app.use(express.bodyParser());
 
-require('./listener')(app);
+app.post('/', function(req, res) {
+   console.log("Received Stuff");
+   console.log(req.body);
+
+   exec('./deploy.sh', function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+   });
+});
 
 app.listen(9999, function(){
   console.log("The github server listening on port 9999" );
