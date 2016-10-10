c6859705cbd2847eb864948911d10dc6f6f832f6
David B. Adrian <dawidh.adrian@googlemail.com>
2016-10-10 21:56:39 +0200

    Initial commit crawling all csv and storing them as csv

--- app/models/locationObject.js
+++ app/models/locationObject.js
@@ -2,12 +2,14 @@
 // grab the mongoose module
 var mongoose = require('mongoose');
 
-// define our nerd model
-// module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('LocationObject', {
+var schema = new mongoose.Schema({
     name : {type : String, default: ''},
     description: {type : String, default: ''},
     lat: {type : Number},
     long: {type : Number},
     district: {type : Number}
 });
+
+// define our nerd model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('LocationObject', schema);
