debfa952ac744a69632b344ca236ef8b91fd0d08
Cecil Woebker <cwoebker@gmail.com>
2016-10-11 18:49:06 +0200

    Get basic angular routing setup to work

--- app/routes.js
+++ app/routes.js
@@ -28,7 +28,7 @@ var passport = require('passport');
         // frontend routes =========================================================
         // route to handle all angular requests
         app.get('/', function(req, res) {
-            res.sendfile('./public/views/index.html'); // load our public/index.html file
+            res.sendfile('./public/views/base.html'); // load our public/index.html file
         });
 
         app.get('/login', function(req, res) {
--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,62 @@
 // app.js
 
 // create the module and name it scotchApp
-var cvApp = angular.module('cvApp', []);
+// also include ngRoute for all our routing needs
+var cvApp = angular.module('cvApp', ['ngRoute']);
+
+// configure our routes
+cvApp.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+        .when('/', {
+            templateUrl : '../views/index.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : '../views/about.html',
+            controller  : 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : '../views/contact.html',
+            controller  : 'contactController'
+        })
+
+        // route for the contact page
+        .when('/login', {
+            templateUrl : '../views/login.html',
+            controller  : 'loginController'
+        })
+
+        // route for the contact page
+        .when('/register', {
+            templateUrl : '../views/register.html',
+            controller  : 'registerController'
+        });
+
+
+});
 
 // create the controller and inject Angular's $scope
 cvApp.controller('mainController', function($scope) {
-
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
+});
+
+cvApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+cvApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
+
+cvApp.controller('loginController', function($scope) {
+});
+
+cvApp.controller('registerController', function($scope) {
 });
