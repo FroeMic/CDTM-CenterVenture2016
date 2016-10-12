f83c6d79aa33a2d759810608fc1ce6c1bbcdecbc
Cecil Woebker <cwoebker@gmail.com>
2016-10-12 21:17:17 +0200

    Add message and user Object from data model

---
+++ app/models/userObject.js
@@ -0,0 +1,20 @@
+/**
+ * Created by cwoebker on 12.10.16.
+ */
+
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    id : {type: String, default: ''},
+    name : {type : String, default: ''},
+    description: {type : String, default: ''},
+    facebook_id: {type : String, default: ''},
+    birthday: {type : Date},
+    occupation: {type: String, default: ''},
+    gender: {type: String, default: ''},
+
+});
+
+// define our nerd model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('UserObject', schema);
