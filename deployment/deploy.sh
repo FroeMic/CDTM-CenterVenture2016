ee986ddfde4f36e5672f43a852d90af10199151e
Michael Froehlich <mike@frhlch.at>
2016-10-12 11:59:19 +0000

    Update deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 pm2 stop server
-pm2 stop webhookListener
 pm2 delete server
-pm2 delete webhookListener
 git reset --hard
 git pull
 echo "module.exports = {url : 'mongodb://localhost:12345/'}" >  ../config/db.js
-pm2 start webhookListener.js
 cd ..
 pm2 start server.js
 
