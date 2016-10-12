2dfe5f1acb268a8ba6850a9933db0d9509bf2999
Benedikt Brandner <benedikt.brandner@gmail.com>
2016-10-12 15:48:19 +0200

    rent niveau routes and frontend

--- app/routes.js
+++ app/routes.js
@@ -37,6 +37,10 @@ module.exports = function(app) {
         res.send(JSON.stringify(
             [
                 {
+                    name: 'Rents',
+                    url: '/map/rentniveau'
+                },
+                {
                     name: 'Test',
                     url: '/dummy/pois'
                 }
@@ -44,8 +48,25 @@ module.exports = function(app) {
         ));
     });
 
-    app.get('/map/voting', function (req, res) {
-        var collectionId = '57fdf5b2fe7db12bf8976827';
+    var mongoose = require('mongoose');
+    var RentModel = require('./models/rentNiveau');
+    var DatasetModel = require('./models/dataset');
+    app.get('/map/rentniveau', function (req, res) {
+        var url = 'http://data.ub.uni-muenchen.de/2/1/miete03.asc';
+        DatasetModel.findOne({url_csv: 'http://data.ub.uni-muenchen.de/2/1/miete03.asc'}, function (err, dataset) {
+            var refId = dataset._id;
+            RentModel.aggregate([
+                { $match: { ods_ref_id: refId } },
+                { $group: { _id: '$district', rent: { $avg: '$value' } } }
+            ], function (err, data) {
+                if(err) {
+                    console.error(err);
+                }
+
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(data));
+            });
+        });
     });
 
     app.get('/dummy/pois', function (req, res) {
